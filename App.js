be5ef8f5d4db92eab58a9505190e5f646ed3f562
aman935464 <aman9354644145@gmail.com>
2026-05-19 18:42:12 +0530

    added get profile features

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import userRouter from "./src/routes/auth.routes.js"
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }))
 app.use(express.json());
