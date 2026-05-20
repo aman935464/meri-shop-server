d6148d21ba5103652a8e07984acd41fc6dce826e
aman935464 <aman9354644145@gmail.com>
2026-05-20 15:22:30 +0530

    added authentication

--- src/middleware/verifyToken.middleware.js
+++ src/middleware/verifyToken.middleware.js
@@ -3,14 +3,14 @@ import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
 
   try {
-    const token = req.cookies.refreshToken;
+    const token = req.cookies.accessToken;
     if(!token){
       return res.status(401).json({
         success: false,
         message: "Unauthorized"
       })
     }
-    const decoded = jwt.verify(token, process.env.REFRESH_TOKEN);
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN);
     req.user = decoded;
     next();
     
